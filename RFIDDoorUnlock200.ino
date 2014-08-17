2ca95b8753d4fa3b279017a54f2625e6b79fa91a
omersiar <osiarbaysal@gmail.com>
2014-08-17 14:53:51 +0300

    New Version Release
    In this version, Master Card's UID must not be hard coded. It will be defined when first use, or if EEPROM wiped.

--- RFIDDoorUnlock200.ino
+++ RFIDDoorUnlock200.ino
@@ -155,28 +155,30 @@ void setup() {
       }
       Serial.println("!!! Wiped !!!");
       wipeModeOn(); // visualize successful wipe
-      digitalWrite(redLed, LOW);
+      digitalWrite(redLed, LED_OFF);
     }
     else {
       Serial.println("!!! Wiping Cancelled !!!");
+      digitalWrite(redLed, LED_OFF);
     }
   }
+  //Check if master card defined, if not let user choose a master card
   if (EEPROM.read(1) == 0) {  // Look EEPROM if Master Card defined, EEPROM address 1 holds if defined
     Serial.println("No Master Card Defined");
     Serial.println("Scan A PICC to Define as Master Card");
-    int successRead; // Variable integer to keep if we have Successful Read from Reader
     do {
       successRead = getID(); // sets successRead to 1 when we get read from reader otherwise 0
-      digitalWrite(blueLed, HIGH); // Visualize Master Card need to be defined
-      delay(200);
-      digitalWrite(blueLed, LOW);
+      digitalWrite(blueLed, LED_ON); // Visualize Master Card need to be defined
+      delay(300);
+      digitalWrite(blueLed, LED_OFF);
+      delay(300);
     }
     while (!successRead); //the program will not go further while you not get a successful read
     for ( int j = 0; j < 4; j++ ) { // Loop 4 times
       EEPROM.write( 3 +j, readCard[j] ); // Write scanned PICC's UID to EEPROM, start from address 3
       EEPROM.write(1,1); //Now Master Card Defined so set it on EEPROM too.
-      Serial.println("Master Card Defined");
     }
+    Serial.println("Master Card Defined");
   }
   Serial.println("##### RFID Door Unlocker #####");
   Serial.println("Master Card's UID");
@@ -256,7 +258,7 @@ int getID() {
   }
   // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
   Serial.println("Scanned PICC's UID:");
-  for (byte i = 0; i < mfrc522.uid.size; i++) {  // for size of uid.size write uid.uidByte to readCard
+  for (int i = 0; i < mfrc522.uid.size; i++) {  // for size of uid.size write uid.uidByte to readCard
     readCard[i] = mfrc522.uid.uidByte[i];
     Serial.print(readCard[i], HEX);
   }
@@ -291,7 +293,7 @@ void normalModeOn () {
 
 //////////////////////////////////////// Read an ID from EEPROM //////////////////////////////
 void readID( int number ) {
-  int start = (number * 4 ) + 2; // Figure out starting position
+  int start = (number * 4 ) + 3; // Figure out starting position
   for ( int i = 0; i < 4; i++ ) { // Loop 4 times to get the 4 Bytes
     storedCard[i] = EEPROM.read(start+i); // Assign values read from EEPROM to array
   }
@@ -301,7 +303,7 @@ void readID( int number ) {
 void writeID( byte a[] ) {
   if ( !findID( a ) ) { // Before we write to the EEPROM, check to see if we have seen this card before!
     int num = EEPROM.read(0); // Get the numer of used spaces, position 0 stores the number of ID cards
-    int start = ( num * 4 ) + 6; // Figure out where the next slot starts
+    int start = ( num * 4 ) + 7; // Figure out where the next slot starts
     num++; // Increment the counter by one
     EEPROM.write( 0, num ); // Write the new count to the counter
     for ( int j = 0; j < 4; j++ ) { // Loop 4 times
@@ -327,7 +329,7 @@ void deleteID( byte a[] ) {
     int j;
     int count = EEPROM.read(0); // Read the first Byte of EEPROM that
     slot = findIDSLOT( a ); //Figure out the slot number of the card to delete
-    start = (slot * 4) + 2;
+    start = (slot * 4) + 3;
     looping = ((num - slot) * 4);
     num--; // Decrement the counter by one
     EEPROM.write( 0, num ); // Write the new count to the counter
