e9a5140e900b49d2e297ed8b893a73000a563e2c
Michael Sauer <sauer.uetersen@gmail.com>
2015-09-01 17:09:51 +0200

    Decreases the size of the Led blinking methods to get smaller and more readable code

--- SD/Leds.ino
+++ SD/Leds.ino
@@ -1,58 +1,44 @@
+#define BLINK_DELAY 200 // Delay for flashing leds (ms)
 ///////////////////////////////////////// Write Success to EEPROM   ///////////////////////////////////
 // Flashes the green LED 3 times to indicate a successful write to EEPROM
 void greenBlink() {
-  digitalWrite(blueLed, LED_OFF); 	// Make sure blue LED is off
-  digitalWrite(redLed, LED_OFF); 	// Make sure red LED is off
-  digitalWrite(greenLed, LED_OFF); 	// Make sure green LED is on
-  delay(200);
-  digitalWrite(greenLed, LED_ON); 	// Make sure green LED is on
-  delay(200);
-  digitalWrite(greenLed, LED_OFF); 	// Make sure green LED is off
-  delay(200);
-  digitalWrite(greenLed, LED_ON); 	// Make sure green LED is on
-  delay(200);
-  digitalWrite(greenLed, LED_OFF); 	// Make sure green LED is off
-  delay(200);
-  digitalWrite(greenLed, LED_ON); 	// Make sure green LED is on
-  delay(200);
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  bool state = true;
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(blueLed, state);   // Toggle green LED
+    state = !state;
+    delay(BLINK_DELAY);
+  }
 }
 
 ///////////////////////////////////////// Write Failed to EEPROM   ///////////////////////////////////
 // Flashes the red LED 3 times to indicate a failed write to EEPROM
 void redBlink() {
-  digitalWrite(blueLed, LED_OFF); 	// Make sure blue LED is off
-  digitalWrite(redLed, LED_OFF); 	// Make sure red LED is off
-  digitalWrite(greenLed, LED_OFF); 	// Make sure green LED is off
-  delay(200);
-  digitalWrite(redLed, LED_ON); 	// Make sure red LED is on
-  delay(200);
-  digitalWrite(redLed, LED_OFF); 	// Make sure red LED is off
-  delay(200);
-  digitalWrite(redLed, LED_ON); 	// Make sure red LED is on
-  delay(200);
-  digitalWrite(redLed, LED_OFF); 	// Make sure red LED is off
-  delay(200);
-  digitalWrite(redLed, LED_ON); 	// Make sure red LED is on
-  delay(200);
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  bool state = true;
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(redLed, state);   // Toggle red LED
+    state = !state;
+    delay(BLINK_DELAY);
+  }
 }
 
 ///////////////////////////////////////// Success Remove UID From EEPROM  ///////////////////////////////////
 // Flashes the blue LED 3 times to indicate a success delete to EEPROM
 void blueBlink() {
-  digitalWrite(blueLed, LED_OFF); 	// Make sure blue LED is off
-  digitalWrite(redLed, LED_OFF); 	// Make sure red LED is off
-  digitalWrite(greenLed, LED_OFF); 	// Make sure green LED is off
-  delay(200);
-  digitalWrite(blueLed, LED_ON); 	// Make sure blue LED is on
-  delay(200);
-  digitalWrite(blueLed, LED_OFF); 	// Make sure blue LED is off
-  delay(200);
-  digitalWrite(blueLed, LED_ON); 	// Make sure blue LED is on
-  delay(200);
-  digitalWrite(blueLed, LED_OFF); 	// Make sure blue LED is off
-  delay(200);
-  digitalWrite(blueLed, LED_ON); 	// Make sure blue LED is on
-  delay(200);
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  bool state = true;
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(blueLed, state);   // Toggle blue LED
+    state = !state;
+    delay(BLINK_DELAY);
+  }
 }
 
 
